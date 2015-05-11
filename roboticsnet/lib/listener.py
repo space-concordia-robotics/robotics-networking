6d60caaffa95ff6dedb7b617193f9fc3278f5824
psyomn <lethaljellybean@gmail.com>
2015-05-10 20:48:17 -0400

    Add lib/

---
+++ roboticsnet/lib/listener.py
@@ -0,0 +1,12 @@
+class Listener:
+    """
+    author: psyomn
+
+    The listener is basically the main entry point for this smaller module
+    for the rover. It is responsible for receiving information, and passing it
+    first to the validator, and then to the dispatcher.
+    """
+
+    def listen(port=5000):
+        """ main entry point """
+        pass
