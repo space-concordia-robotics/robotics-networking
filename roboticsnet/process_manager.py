f125eb9f954c8a9503ae93577c4eca4286e1c826
psyomn <lethaljellybean@gmail.com>
2015-05-12 15:43:17 -0400

    process_manager: add stubs for proc man

---
+++ roboticsnet/process_manager.py
@@ -0,0 +1,13 @@
+class ProcessManager:
+    """
+    This is the process manager. It should deal with:
+      - spawning required processes at startup
+      - monitoring running processes after startup
+      - restarting killed processes if there is any issue
+      - share information about such processes
+
+    author: psyomn
+    """
+
+    def __init__(self):
+        pass
