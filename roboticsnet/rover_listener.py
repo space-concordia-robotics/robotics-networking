09d2cc09e8cfba41bfe433e67dc36158b2f92c9c
psyomn <lethaljellybean@gmail.com>
2015-05-13 00:16:13 -0400

    rover_listener: add graceful shutdown
    Issue #5

--- roboticsnet/rover_listener.py
+++ roboticsnet/rover_listener.py
@@ -2,6 +2,7 @@ from multiprocessing.connection import Listener
 
 from roboticsnet.commands.command_factory import CommandFactory
 from roboticsnet.sanitizer import sanitize
+import roboticsnet.gateway_constants
 
 class RoverListener:
     """
@@ -14,6 +15,7 @@ class RoverListener:
 
     def __init__(self, default_port=5000):
         self.port = default_port
+        self.end_listen = False
 
     def listen(self):
         """ main entry point """
@@ -24,8 +26,12 @@ class RoverListener:
         l = Listener(address)
 
         # TODO add graceful shutdown?
-        while True:
+        while not self.end_listen:
             conn = l.accept()
-            print "Received: ", conn.recv_bytes()
+            bytes = conn.recv_bytes()
+            print "Received: ", bytes
             conn.close()
+            if bytes[0] == ROBOTICSNET_COMMAND_GRACEFUL:
+                self.end_listen = True
+        print "BYE."
 
