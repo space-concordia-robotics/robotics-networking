d630b209aefac3543fbafb82d88bfa5ae903e7f0
psyomn <lethaljellybean@gmail.com>
2015-06-02 02:36:46 -0400

    command_factory: can create reverse commands
    Issue #13

--- roboticsnet/commands/command_factory.py
+++ roboticsnet/commands/command_factory.py
@@ -1,6 +1,7 @@
 from roboticsnet.commands.command_validator import validate
 from roboticsnet.commands.move_command import MoveCommand
 from roboticsnet.commands.turn_command import TurnCommand
+from roboticsnet.commands.reverse_command import ReverseCommand
 from roboticsnet.commands.queryproc_command import QueryprocCommand
 from roboticsnet.gateway_constants import *
 
@@ -29,15 +30,15 @@ class CommandFactory:
         params = rcv_bytes[1:]
 
         if cmd == ROBOTICSNET_COMMAND_MOVE:
-            print "Make move command"
             return CommandFactory._makeMove(params)
 
+        elif cmd == ROBOTICSNET_COMMAND_REVERSE:
+            return CommandFactory._makeReverse(params)
+
         elif cmd == ROBOTICSNET_COMMAND_TURN:
-            print "Turn stuff"
             return CommandFactory._makeTurn(params)
 
         elif cmd == ROBOTICSNET_COMMAND_QUERYPROC:
-            print "Query running processes"
             return QueryprocCommand(conn, session)
 
     @staticmethod
@@ -49,4 +50,9 @@ class CommandFactory:
     def _makeTurn(rcv_bytes):
         magnitude = ord(rcv_bytes[0])
         return TurnCommand(magnitude)
+
+    @staticmethod
+    def _makeReverse(rcv_bytes):
+        magnitude = ord(rcv_bytes[0])
+        return ReverseCommand(magnitude)
 
