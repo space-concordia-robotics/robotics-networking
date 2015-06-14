12424c3ba09c5ea1e3b8f362b2cca1f20bc7be2f
psyomn <lethaljellybean@gmail.com>
2015-06-13 22:40:18 -0400

    commands: start video; add mental notes

--- roboticsnet/commands/start_video_command.py
+++ roboticsnet/commands/start_video_command.py
@@ -2,4 +2,11 @@ from roboticsnet.commands.commandable import Commandable
 
 class StartVideoCommand(Commandable):
     """ Sends a request to boot the video process """
-    pass
+
+    # TODO complete this
+    def __init__():
+        pass
+
+    # TODO complete this
+    def execute():
+        pass
