34badf4fb68728e9c92a7b9ee70d30ba952a32fb
psyomn <lethaljellybean@gmail.com>
2015-06-13 22:40:00 -0400

    commands: add stubs for stop video

---
+++ roboticsnet/commands/stop_video_command.py
@@ -0,0 +1,11 @@
+class StopVideoCommand(Commandable):
+    """ Sends stuff to the server, to tell it to stop doing video command
+    stuff """
+
+    # TODO: need to complete this
+    def init():
+        pass
+
+    # TODO: need to complete this
+    def execute():
+        pass
