11016931471b1f8b5be1e3acfe3f0ec56e38b435
psyomn <lethaljellybean@gmail.com>
2015-10-05 03:00:33 -0400

    examples: monitoring service example works
    Right now this is incomplete. We simply need to have an interface that returns
    all the values from the polled services

--- examples/service_example.py
+++ examples/service_example.py
@@ -20,19 +20,30 @@ def _forwardHook():
 
 def polling_service():
     """ Returns the same number all the time; for testing purposes """
+    print "polling service 1 is executed!"
     return 42
 
 def polling_service_2():
     """ Just another service """
+    print "Service 2!"
     return 24
+
+def cat():
+    """ somehow a cat made it into the software! """
+    print "MEW MEW MEW MEW"
+    return 'cat'
 
 # First you would need to define your hooks using CommandHook
 cmd_hook = CommandHook(
         forward=_forwardHook
         )
 
-l = RoverListener(hooks=cmd_hook,
-        monitorProcs=[polling_service, polling_service_2])
+l = RoverListener(\
+        hooks=cmd_hook,
+        monitorProcs=[\
+            polling_service,
+            polling_service_2,
+            cat])
 
 print roboticsnet.__appname__, " ",  roboticsnet.__version__
 print "Starting command dispatcher..."
