c88e8ff78dfed5af524df4e4948d36a256dd9ccc
Arielle Evans <ariellelevans@gmail.com>
2016-03-03 18:54:21 -0500

    re-added functionalities that I removed (like ping)

--- roboticsnet/rover_client.py
+++ roboticsnet/rover_client.py
@@ -14,7 +14,7 @@ class RoverClient:
     """
 
     def __init__(self):
-        self.ser = serial.Serial('/dev/ttyUSB1', 9600, timeout=1)
+        self.ser = serial.Serial('/dev/ttyUSB1', 9600, timeout=None)
 
     def sendCommand(self, command):
         """ Sends a request to the server """
@@ -31,4 +31,51 @@ class RoverClient:
         self._sendMessage(message) #sent with udp
 
     def _sendMessage(self, message):
+        """Writing to the serial port.
+           The \n is so that ser.readline works on the other end"""
         self.ser.write(message+"\n")
+
+
+    def query(self):
+        """ Issue a queryproc request """
+        message = RoverUtils.hexArr2Str([SYSTEM_QUERYPROC])
+        return self._sendMessageAwaitReply(message)
+
+    def sendCommand(self, command):
+        """ Sends a request to the server """
+        message = RoverUtils.hexArr2Str([command])
+        self._sendMessage(message)
+
+    def timedCommand(self, command, magnitude=0):
+        """ Sends a request to the server """
+        """ This is only for movement commands """
+        if not magnitude in range(0, 65):
+            raise RoboticsnetException("You can send things in range of 0 to 255 only")
+        message = RoverUtils.hexArrToTimestampedString([command, magnitude])
+        self._sendMessage(message)
+
+
+    def _sendMessageAwaitReply(self, message):
+        """
+        As _sendMessage, but this method waits for a reply
+        Parameters:
+            message - is the bytearray to send, in string format
+        """
+        self.ser.write(message+"\n")
+        """So it times out if nothing is received"""
+        ser.setTimeout(3000)
+        data = self.ser.readline()
+        ser.setTimeout(None)
+        if len(data)==0:
+            return None
+        return data
+
+    def sensInfo(self):
+        """ Request information about sensors """
+        message = RoverUtils.hexArr2Str([SENSOR_INFO])
+        return self._sendMessageAwaitReply(message)
+
+    def ping(self):
+        """ Pings the rover with a timestamp """
+        message = RoverUtils.hexArrToTimestampedString([SYSTEM_PING])
+        return self._sendMessageAwaitReply(message)
--- roboticsnet/rover_listener.py
+++ roboticsnet/rover_listener.py
@@ -36,11 +36,53 @@ class RoverListener():
                 received_bytes = self.ser.readline()
                 readable = RoverUtils.hexArrToHumanReadableString(received_bytes)
                 print "received",readable
-
+                
                 if ord(received_bytes[0]) == SYSTEM_GRACEFUL:
-                    message = RoverUtils.hexArr2Str([SYSTEM_GRACEFUL])
                     self.end_listen = True
+                elif ord(received_bytes[0]) == SYSTEM_PING:
+                    diff = calculate_time_diff(ord(received_bytes[1]))
+                    logging.info("Received ping from {0} in {1}s".format(addr, diff))
+                    self.ser.write(str(diff)+"\n")
                 else:
                     self.commandable.execute(received_bytes)
-            except Exception as e:
-                print e.message
+
+            except AttributeError as e:
+                logging.error("Attribute error on commandable execute. This is most likely because there is no commandable file to execute:\n\t{0}".format(e.message))
+            except:
+                logging.error("There was some error. Ignoring last command")
+                logging.error(sys.exc_info()[0])
+                logging.error(traceback.format_exc())
+
+        self.ser.close()
+        self._stopRunningServices()
+        print "Server closed."
+
+    def _stopRunningServices(self):
+        """ If there exists any running services (like sensor polling
+        functions), this method will stop them """
+        print "Attempting to stop services"
+        print self.monitorServices
+        for service in self.monitorServices:
+            print "Send stop to: ", service
+            service.stop()
+        for service in self.monitorServices:
+            print "Join: ", service
+            service.join()
+
+    def stop(self):
+        self.end_listen = True
+
+
+    def _spawnMonitoringServices(self, monitorProcs):
+        """ This starts all the monitoring services (as threads) """
+        if not monitorProcs:
+            return
+
+        for lamb in monitorProcs:
+            print "Init polling service [", lamb.func_name, "]"
+            print "  [Service Info] ", lamb.__doc__
+            monServ = MonitoringService(0, lamb)
+            self.monitorServices.append(monServ)
+            monServ.start()
+
+        print "All services started"
