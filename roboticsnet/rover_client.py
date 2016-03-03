0afea7ea352f6b0e02385e4724b4f6c8c0ebaf48
Arielle Evans <ariellelevans@gmail.com>
2016-03-03 17:37:48 -0500

    temporary changes to get it working for demo

--- roboticsnet/rover_client.py
+++ roboticsnet/rover_client.py
@@ -1,9 +1,10 @@
 from roboticsnet.gateway_constants import *
 from roboticsnet.rover_utils import RoverUtils
 from roboticsnet.roboticsnet_exception import RoboticsnetException
-import socket
 from colorama import Fore
+import serial
 import sys
+
 class RoverClient:
     """
     The client that interfaces to the Rover gateway server. This is where we add
@@ -12,38 +13,14 @@ class RoverClient:
     author: psyomn
     """
 
-    def __init__(self, host='localhost', tcp_port=TCP_PORT, udp_port=UDP_PORT):
-        self.host = host
-        self.tcp_port = tcp_port
-        self.udp_port = udp_port
-
-    def getPort(self, TCP = True):
-        if (TCP):
-            return self.tcp_port
-        else:
-            return self.udp_port
-
-    def getHost(self):
-        return self.host
-
-    def setHost(self, host):
-        self.host = host
-
-    def setPort(self, port, TCP = True):
-        if (TCP):
-            self.tcp_port = port
-        else:
-            self.udp_port = port
-
-    def query(self):
-        """ Issue a queryproc request """
-        message = RoverUtils.hexArr2Str([SYSTEM_QUERYPROC])
-        return self._sendMessageAwaitReply(message)
+    def __init__(self):
+        self.ser = serial.Serial('/dev/ttyUSB1', 9600, timeout=1)
 
     def sendCommand(self, command):
         """ Sends a request to the server """
         message = RoverUtils.hexArr2Str([command])
         self._sendMessage(message)
+
 
     def timedCommand(self, command, magnitude=0):
         """ Sends a request to the server """
@@ -51,91 +28,7 @@ class RoverClient:
         if not magnitude in range(0, 65):
             raise RoboticsnetException("You can send things in range of 0 to 65 only")
         message = RoverUtils.hexArrToTimestampedString([command, magnitude])
-        self._sendMessage(message, False) #sent with udp
+        self._sendMessage(message) #sent with udp
 
-    def _sendMessage(self, message, TCP=True):
-        """
-        Given the host, and port, we build the connection. Don't call this
-        outside the constructor, unless you know what you are doing.
-
-        Parameters:
-            message - is the bytearray to send, in string format
-            TCP     - whether or not it will be sent with TCP. true by default
-        """
-        if TCP:
-            address = (self.host, self.tcp_port)
-
-            print "Using port: " + Fore.GREEN, self.tcp_port, Fore.RESET
-            print "Using host: " + Fore.GREEN, self.host, Fore.RESET
-
-            #from TCPCommunication on the Python wiki
-            tsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            tsock.settimeout(3)
-            tsock.connect(address)
-            tsock.send(message)
-            tsock.shutdown(socket.SHUT_WR)
-            tsock.close()
-        else:
-            #from UdpCommunication on the Python wiki
-            print "Using port: " + Fore.GREEN, self.udp_port, Fore.RESET
-            print "Using host: " + Fore.GREEN, self.host, Fore.RESET
-
-            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            sock.sendto(message, (self.host,self.udp_port))
-    
-    def snapshot(self, command):
-        message = RoverUtils.hexArr2Str([command])
-        buffer_size=921600
-        address = (self.host, self.tcp_port)
-        tsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        tsock.settimeout(3)
-        try:
-            tsock.connect(address)
-            tsock.send(message)
-            data = ""
-            while len(data) < buffer_size:
-                packet = tsock.recv(buffer_size - len(data))
-                if not packet:
-                    return None
-                data += packet
-            return data
-        except Exception as e:
-            print e
-            data = None
-        finally:
-            tsock.shutdown(socket.SHUT_WR)
-            tsock.close()
-            return data
-
-    def _sendMessageAwaitReply(self, message, buffer_size = 1024):
-        """
-        As _sendMessage, but this method waits for a reply
-
-        Parameters:
-            message - is the bytearray to send, in string format
-        """
-        address = (self.host, self.tcp_port)
-        tsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        tsock.settimeout(3)
-        try:
-            tsock.connect(address)
-            tsock.send(message)
-            data = tsock.recv(buffer_size)
-            return data
-        except Exception as e:
-            print e
-            data = None
-        finally:
-            tsock.shutdown(socket.SHUT_WR)
-            tsock.close()
-            return data
-
-    def sensInfo(self):
-        """ Request information about sensors """
-        message = RoverUtils.hexArr2Str([SENSOR_INFO])
-        return self._sendMessageAwaitReply(message)
-
-    def ping(self):
-        """ Pings the rover with a timestamp """
-        message = RoverUtils.hexArrToTimestampedString([0xF1])
-        return self._sendMessageAwaitReply(message)
+    def _sendMessage(self, message):
+        self.ser.write(message+"\n")
--- roboticsnet/rover_listener.py
+++ roboticsnet/rover_listener.py
@@ -1,8 +1,8 @@
 import sys
 import traceback
 import threading
-import socket
 import logging
+import serial
 
 from multiprocessing import Process, Pipe
 from colorama import Fore
@@ -23,113 +23,24 @@ class RoverListener():
 
     def __init__(self, default_port=TCP_PORT,
             monitorProcs=None, hook=None):
-        """
-        default_port:
-            The port that the server monitors on in default.
 
-        hook:
-            This is really just a placeholder name for the initialization of the Commands class the listener uses.
-
-        monitorProcs:
-            An array of lambdas, which have arity of 1 (they take in one
-            parameter).
-
-
-        author: psyomn
-        """
-        self.port = default_port
+        self.ser = serial.Serial('/dev/ttyUSB0', 9600,timeout=None)
         self.end_listen = False
-        self.monitorServices = []
-        self._spawnMonitoringServices(monitorProcs)
         self.commandable = hook #again, just a placeholder name. could be changed
         logging.basicConfig(filename='rover_listener.log',level=logging.DEBUG)
 
 
     def start(self):
-
-        """ main entry point """
-        logging.info("TCP listening on port: %d" % (self.port))
-        print "TCP listening on port: %d" % (self.port)
-
-        address = ('', self.port)
-
-        s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        s.bind(address)
-        s.listen(1)
-
-        """To kill the Udp listener when this one receives graceful"""
-        sock2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
         while not self.end_listen:
             try:
-                conn, addr = s.accept()
-                received_bytes = conn.recv(1024)
+                received_bytes = self.ser.readline()
                 readable = RoverUtils.hexArrToHumanReadableString(received_bytes)
-                logging.info("{0}: {1}".format(addr, readable))
                 print "received",readable
 
                 if ord(received_bytes[0]) == SYSTEM_GRACEFUL:
                     message = RoverUtils.hexArr2Str([SYSTEM_GRACEFUL])
-                    sock2.sendto(message, ("localhost",10667))
                     self.end_listen = True
-                elif ord(received_bytes[0]) == SYSTEM_PING:
-                    diff = calculate_time_diff(ord(received_bytes[1]))
-                    logging.info("Received ping from {0} in {1}s".format(addr, diff))
-                    conn.send(str(diff))
-                elif ord(received_bytes[0]) == CAMERA_SNAPSHOT:
-                    logging.info("Received snapshot from {0}".format(addr))
-                    img = self.commandable.execute(received_bytes)
-                    chunk = 4096*4
-                    for i in range(0,len(img),chunk):
-                        if i+chunk<len(img):
-                            temp = img[i:i+chunk]
-                        else:
-                            temp = img[i:len(img)]
-                        conn.send(temp)
                 else:
                     self.commandable.execute(received_bytes)
-
-            except AttributeError as e:
-                logging.error("Attribute error on commandable execute. This is most likely because there is no commandable file to execute:\n\t{0}".format(e.message))
-            except:
-                logging.error("There was some error. Ignoring last command")
-                logging.error(sys.exc_info()[0])
-                logging.error(traceback.format_exc())
-
-            finally:
-                """ Conn might not be set if nothing is received """
-                if 'conn' in vars() or 'conn' in globals():
-                    conn.close()
-
-        conn.close()
-        self._stopRunningServices()
-        print "Server closed."
-
-    def _stopRunningServices(self):
-        """ If there exists any running services (like sensor polling
-        functions), this method will stop them """
-        print "Attempting to stop services"
-        print self.monitorServices
-        for service in self.monitorServices:
-            print "Send stop to: ", service
-            service.stop()
-        for service in self.monitorServices:
-            print "Join: ", service
-            service.join()
-
-    def stop(self):
-        self.end_listen = True
-
-    def _spawnMonitoringServices(self, monitorProcs):
-        """ This starts all the monitoring services (as threads) """
-        if not monitorProcs:
-            return
-
-        for lamb in monitorProcs:
-            print "Init polling service [", lamb.func_name, "]"
-            print "  [Service Info] ", lamb.__doc__
-            monServ = MonitoringService(0, lamb)
-            self.monitorServices.append(monServ)
-            monServ.start()
-
-        print "All services started"
+            except Exception as e:
+                print e.message
